fb4a7dbc6ad4e3c358e5f6eb07bac6ded8310328
sunhui04 <sunhui04@meituan.com>
2017-02-06 18:29:04 +0800

    update

--- promise.js
+++ promise.js
@@ -5,7 +5,7 @@ const RESOLVED = 'resolved';
 const REJECTED = 'rejected';
 const UNDEFINED = void 0;
 
-
+//定义promise构造函数
 function Promise (resolver) {
 	if(!resolver || typeof resolver !== 'function') {
 		return;
@@ -21,7 +21,26 @@ function Promise (resolver) {
 	}
 }
 
-Promise.prototype.then = function () {
+//then方法，必须返回一个新的promise实例
+//
+Promise.prototype.then = function (onResolved, onRejected) {
+	//只要状态变化了，但是参数不是函数，忽略
+	if(typeof onResolved !== 'function' && this.state === RESOLVED ||
+		typeof onRejected !== 'function' && this.state === REJECTED) {
+		return this;
+	}
+
+	let promise = new this.constructor();
+
+	if(this.state !== PENDING) {
+		let callback = this.state === RESOLVED ? onResolved : onRejected;
+		executeCallbackAsync.call(promise, callback, this.data);
+	} else {
+		this.callbackQueue.forEach(v => v[type](x));
+	}
+
+	return promise;
+
 
 }
 
@@ -54,10 +73,10 @@ function executeResolver (resolver) {
 	}
 }
 
-function execuCallback(type, x) {
+function executeCallback(type, x) {
 	let isResolve = type === 'resolve';
 	let thenable;
-
+	//如果x是一个对象或者函数
 	if( isResolve && (typeof x === 'object' || typeof x === 'function') ) {
 		try {
 			thenable = getThen(x);
@@ -67,7 +86,7 @@ function execuCallback(type, x) {
 	}
 
 	if(isResolve && thenable) {
-		executeCallback.call(this, thenable);
+		executeResolver.call(this, thenable);
 	} else {
 		this.state = isResolve ? RESOLVED : REJECTED;
 		this.data = x;
@@ -77,6 +96,38 @@ function execuCallback(type, x) {
 	}
 	return this;
 }
+
+function getThen(obj) {
+	let then = obj && obj.then;
+	if(obj && typeof obj === 'object' && typeof then === 'function') {
+		return function applyThen() {
+			then.apply(obj, arguments);
+		}
+	}
+}
+
+//异步执行then方法中的回调函数
+function executeCallbackAsync(callback, value) {
+	let _this = this;
+	setTimeout(function () {
+		let res;
+		try {
+			res = callback(value);
+		} catch(err) {
+			return executeCallback.call(this, 'reject', err);
+		}
+
+		if(res !== _this) {
+			return executeCallback.call(_this, 'resolve', res);
+		} else {
+			return executeCallback.call(_this, 'reject', new TypeError('error'));
+		}
+	}, 1);
+}
+
+
+
+
 
 
 
